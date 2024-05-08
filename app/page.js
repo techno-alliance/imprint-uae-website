a3cc8619e8315c26780c9e616c7403c7e98b294e
abishek180 <anilabishek15@gmail.com>
2024-05-08 12:42:46 +0530

    major changes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import Hero from "@/components/New/Hero";
 import MovingImage from "@/components/New/MovingImage";
 import { BentoGridDemo } from "@/components/New/BentoGridDemo";
@@ -10,6 +9,61 @@ import FeaturedProjects from "@/components/New/FeaturedProjects";
 import FeaturedProjects1 from "@/components/New/FeaturedProjects1";
 import Testimonial from "@/components/New/Testimonial";
 import Blog from "@/components/New/Blog";
+import React from "react"
+import { StickyScroll } from "../components/Sparkle/Ui/StickyScroll"
+import Image from "next/image"
+import { StickyScrollRevealDemo } from "@/components/New/StickyScrollDemo";
+
+// const content = [
+//   {
+//     title: "Collaborative Editing",
+//     description:
+//       "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+//     content: (
+//       <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+//         Collaborative Editing
+//       </div>
+//     )
+//   },
+//   {
+//     title: "Real time changes",
+//     description:
+//       "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+//     content: (
+//       <div className="h-full w-full  flex items-center justify-center text-white">
+//         <Image
+//           src="/linear.webp"
+//           width={300}
+//           height={300}
+//           className="h-full w-full object-cover"
+//           alt="linear board demo"
+//         />
+//       </div>
+//     )
+//   },
+//   {
+//     title: "Version control",
+//     description:
+//       "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+//     content: (
+//       <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
+//         Version control
+//       </div>
+//     )
+//   },
+//   {
+//     title: "Running out of content",
+//     description:
+//       "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+//     content: (
+//       <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+//         Running out of content
+//       </div>
+//     )
+//   }
+// ]
+
+
 
 export default function Home() {
   // const container = useRef(null);
@@ -271,7 +325,11 @@ export default function Home() {
           <Slider />
         </div>
       </section>
-      {/* <FeaturedProjects1/> */}
+      {/* <div className="py-20">
+      <StickyScroll content={content} />
+      </div> */}
+      {/* <StickyScrollRevealDemo/> */}
+      {/* <FeaturedProjects/> */}
       <section className="bg-black py-10 lg:py-20">
         <div className="max-w-screen-xl mx-auto px-5 lg:px-10 2xl:px-0">
           <Testimonial />
@@ -290,3 +348,10 @@ export default function Home() {
     </div>
   );
 }
+// export function StickyScrollRevealDemo() {
+//   return (
+//     <div className="p-10">
+//       <StickyScroll content={content} />
+//     </div>
+//   )
+// }
