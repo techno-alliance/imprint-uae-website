4357c747caa876d4e994a0e04e83d7e1eaa92141
abishek180 <anilabishek15@gmail.com>
2024-05-09 11:10:09 +0530

    minor change in testimonial

--- components/New/Testimonial.jsx
+++ components/New/Testimonial.jsx
@@ -8,6 +8,7 @@ import project1 from "@/public/images/project1.png";
 import project4 from "@/public/images/project4.png";
 import book from "@/public/images/books1.png";
 import { FaPhone } from "react-icons/fa6";
+import Ellipse from "@/public/images/Ellipse.png"
 
 // Import Swiper styles
 import "swiper/css";
@@ -38,11 +39,11 @@ export default function Testimonial() {
             <div className="flex flex-col md:flex-row justify-between gap-5 lg:gap-10 w-full">
               <div className="w-auto lg:w-[400px]">
                 <p className="text-[36px] lg:text-[48px] text-center lg:text-left leading-[140%] font-semibold text-white">
-                Our customers love what we do
+                  Our customers love what we do
                 </p>
               </div>
               <div className="flex justify-center items-center">
-              <div className='h-[80px] bg-[#737373] w-[1px]'/>
+                <div className="h-[80px] bg-[#737373] w-[1px]" />
               </div>
               <div className="flex justify-center items-center w-auto lg:w-[360px]">
                 <p className="text-[#DADADA] text-center lg:text-left text-xl font-normal leading-[160%]">
@@ -52,15 +53,15 @@ export default function Testimonial() {
                 </p>
               </div>
               <div className="flex justify-center items-center">
-              <button
+                <button
                   className="button-57 flex justify-center items-center gap-3 transition duration-300 py-1 w-max rounded-full text-[16px] lg:text-[20px] px-1 pr-4 text-white"
                   href=""
                 >
                   <div className="bg-black rounded-full w-12 h-12 text-2xl text-white flex justify-center items-center">
-                  <FaPhone />
+                    <FaPhone />
                   </div>
                   <span className="text-btn flex gap-2 items-center">
-                  Make a Call?
+                    Make a Call?
                   </span>
                   <span className="pb-3">Make a Call?</span>
                 </button>
@@ -71,8 +72,8 @@ export default function Testimonial() {
       </div>
       <Swiper
         slidesPerView={2.5}
-        spaceBetween={20}  
-        centeredSlides={true}  
+        spaceBetween={20}
+        centeredSlides={true}
         // loop={true}
         // pagination={{
         //   clickable: true,
@@ -114,167 +115,192 @@ export default function Testimonial() {
         </div>
         <SwiperSlide className="!p-3 lg:!p-0 !bg-black">
           <div className="!flex !flex-col lg:!flex-row !justify-between gap-5 lg:gap-0 rounded-[10px] !p-3 !w-full !h-full lg:!w-[700px] lg:!h-[300px] !overflow-hidden !relative group !bg-[#1E1E1E]">
-          <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full">
-            <div className="flex justify-start">
-              <p className="text-sm font-extrabold text-white">
-                Etisalat Telecom LLC
+            <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full z-10">
+              <div className="flex justify-start">
+                <p className="text-sm font-extrabold text-white">
+                  Etisalat Telecom LLC
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
+                  <Image
+                    src={project4}
+                    layout="responsive"
+                    className="w-full h-full object-cover object-center"
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p className="text-white text-xl font-semibold">
+                    Jacob Woods
+                  </p>
+                  <p className="text-white text-[16px]">Chief Executive</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
+              <p className="text-[#6A6A6A] font-semibold text-left text-xl">
+                <span className=" text-white font-semibold text-xl">
+                  “ Get speed and consistency
+                </span>
+                —our global in-house and partner facilities offer competitive
+                shipping rates and consistent quality.
               </p>
             </div>
-            <div className="flex gap-2">
-              <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
-                <Image
-                  src={project4}
-                  layout="responsive"
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <p className="text-white text-xl font-semibold">Jacob Woods</p>
-                <p className="text-white text-[16px]">Chief Executive</p>
-              </div>
+            <div className="absolute left-[0%] bottom-[-3%] hidden lg:block">
+              <Image className="w-full h-full object-center object-cover" src={Ellipse}/>
             </div>
-          </div>
-          <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
-            <p className="text-[#6A6A6A] font-semibold text-left text-xl">
-              <span className=" text-white font-semibold text-xl">
-                “ Get speed and consistency
-              </span>
-              —our global in-house and partner facilities offer competitive
-              shipping rates and consistent quality.
-            </p>
-          </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="!p-3 lg:!p-0 !bg-black">
           <div className="!flex !flex-col lg:!flex-row !justify-between gap-5 lg:gap-0 rounded-[10px] !p-3 !w-full !h-full lg:!w-[700px] lg:!h-[300px] !overflow-hidden !relative group !bg-[#1E1E1E]">
-          <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full">
-            <div className="flex justify-start">
-              <p className="text-sm font-extrabold text-white">
-                Etisalat Telecom LLC
+            <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full z-10">
+              <div className="flex justify-start">
+                <p className="text-sm font-extrabold text-white">
+                  Etisalat Telecom LLC
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
+                  <Image
+                    src={project4}
+                    layout="responsive"
+                    className="w-full h-full object-cover object-center"
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p className="text-white text-xl font-semibold">
+                    Jacob Woods
+                  </p>
+                  <p className="text-white text-[16px]">Chief Executive</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
+              <p className="text-[#6A6A6A] font-semibold text-left text-xl">
+                <span className=" text-white font-semibold text-xl">
+                  “ Get speed and consistency
+                </span>
+                —our global in-house and partner facilities offer competitive
+                shipping rates and consistent quality.
               </p>
             </div>
-            <div className="flex gap-2">
-              <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
-                <Image
-                  src={project4}
-                  layout="responsive"
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <p className="text-white text-xl font-semibold">Jacob Woods</p>
-                <p className="text-white text-[16px]">Chief Executive</p>
-              </div>
+            <div className="absolute left-[0%] bottom-[-3%] hidden lg:block">
+              <Image className="w-full h-full object-center object-cover" src={Ellipse}/>
             </div>
-          </div>
-          <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
-            <p className="text-[#6A6A6A] font-semibold text-left text-xl">
-              <span className=" text-white font-semibold text-xl">
-                “ Get speed and consistency
-              </span>
-              —our global in-house and partner facilities offer competitive
-              shipping rates and consistent quality.
-            </p>
-          </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="!p-3 lg:!p-0 !bg-black">
           <div className="!flex !flex-col lg:!flex-row !justify-between gap-5 lg:gap-0 rounded-[10px] !p-3 !w-full !h-full lg:!w-[700px] lg:!h-[300px] !overflow-hidden !relative group !bg-[#1E1E1E]">
-          <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full">
-            <div className="flex justify-start">
-              <p className="text-sm font-extrabold text-white">
-                Etisalat Telecom LLC
+            <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full z-10">
+              <div className="flex justify-start">
+                <p className="text-sm font-extrabold text-white">
+                  Etisalat Telecom LLC
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
+                  <Image
+                    src={project4}
+                    layout="responsive"
+                    className="w-full h-full object-cover object-center"
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p className="text-white text-xl font-semibold">
+                    Jacob Woods
+                  </p>
+                  <p className="text-white text-[16px]">Chief Executive</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
+              <p className="text-[#6A6A6A] font-semibold text-left text-xl">
+                <span className=" text-white font-semibold text-xl">
+                  “ Get speed and consistency
+                </span>
+                —our global in-house and partner facilities offer competitive
+                shipping rates and consistent quality.
               </p>
             </div>
-            <div className="flex gap-2">
-              <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
-                <Image
-                  src={project4}
-                  layout="responsive"
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <p className="text-white text-xl font-semibold">Jacob Woods</p>
-                <p className="text-white text-[16px]">Chief Executive</p>
-              </div>
+            <div className="absolute left-[0%] bottom-[-3%] hidden lg:block">
+              <Image className="w-full h-full object-center object-cover" src={Ellipse}/>
             </div>
-          </div>
-          <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
-            <p className="text-[#6A6A6A] font-semibold text-left text-xl">
-              <span className=" text-white font-semibold text-xl">
-                “ Get speed and consistency
-              </span>
-              —our global in-house and partner facilities offer competitive
-              shipping rates and consistent quality.
-            </p>
-          </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="!p-3 lg:!p-0 !bg-black">
           <div className="!flex !flex-col lg:!flex-row !justify-between gap-5 lg:gap-0 rounded-[10px] !p-3 !w-full !h-full lg:!w-[700px] lg:!h-[300px] !overflow-hidden !relative group !bg-[#1E1E1E]">
-          <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full">
-            <div className="flex justify-start">
-              <p className="text-sm font-extrabold text-white">
-                Etisalat Telecom LLC
+            <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full z-10">
+              <div className="flex justify-start">
+                <p className="text-sm font-extrabold text-white">
+                  Etisalat Telecom LLC
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
+                  <Image
+                    src={project4}
+                    layout="responsive"
+                    className="w-full h-full object-cover object-center"
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p className="text-white text-xl font-semibold">
+                    Jacob Woods
+                  </p>
+                  <p className="text-white text-[16px]">Chief Executive</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
+              <p className="text-[#6A6A6A] font-semibold text-left text-xl">
+                <span className=" text-white font-semibold text-xl">
+                  “ Get speed and consistency
+                </span>
+                —our global in-house and partner facilities offer competitive
+                shipping rates and consistent quality.
               </p>
             </div>
-            <div className="flex gap-2">
-              <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
-                <Image
-                  src={project4}
-                  layout="responsive"
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <p className="text-white text-xl font-semibold">Jacob Woods</p>
-                <p className="text-white text-[16px]">Chief Executive</p>
-              </div>
+            <div className="absolute left-[0%] bottom-[-3%] hidden lg:block">
+              <Image className="w-full h-full object-center object-cover" src={Ellipse}/>
             </div>
-          </div>
-          <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
-            <p className="text-[#6A6A6A] font-semibold text-left text-xl">
-              <span className=" text-white font-semibold text-xl">
-                “ Get speed and consistency
-              </span>
-              —our global in-house and partner facilities offer competitive
-              shipping rates and consistent quality.
-            </p>
-          </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="!p-3 lg:!p-0 !bg-black">
           <div className="!flex !flex-col lg:!flex-row !justify-between gap-5 lg:gap-0 rounded-[10px] !p-3 !w-full !h-full lg:!w-[700px] lg:!h-[300px] !overflow-hidden !relative group !bg-[#1E1E1E]">
-          <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full">
-            <div className="flex justify-start">
-              <p className="text-sm font-extrabold text-white">
-                Etisalat Telecom LLC
+            <div className="flex flex-col gap-5 lg:gap-0 justify-between w-full h-full z-10">
+              <div className="flex justify-start">
+                <p className="text-sm font-extrabold text-white">
+                  Etisalat Telecom LLC
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
+                  <Image
+                    src={project4}
+                    layout="responsive"
+                    className="w-full h-full object-cover object-center"
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p className="text-white text-xl font-semibold">
+                    Jacob Woods
+                  </p>
+                  <p className="text-white text-[16px]">Chief Executive</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
+              <p className="text-[#6A6A6A] font-semibold text-left text-xl">
+                <span className=" text-white font-semibold text-xl">
+                  “ Get speed and consistency
+                </span>
+                —our global in-house and partner facilities offer competitive
+                shipping rates and consistent quality.
               </p>
             </div>
-            <div className="flex gap-2">
-              <div className="w-14 h-14 overflow-hidden rounded-full flex justify-center">
-                <Image
-                  src={project4}
-                  layout="responsive"
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <p className="text-white text-xl font-semibold">Jacob Woods</p>
-                <p className="text-white text-[16px]">Chief Executive</p>
-              </div>
+            <div className="absolute left-[0%] bottom-[-3%] hidden lg:block">
+              <Image className="w-full h-full object-center object-cover" src={Ellipse}/>
             </div>
-          </div>
-          <div className="bg-[#222627] rounded-[10px] w-auto lg:w-[400px] p-4 h-full flex justify-center items-center">
-            <p className="text-[#6A6A6A] font-semibold text-left text-xl">
-              <span className=" text-white font-semibold text-xl">
-                “ Get speed and consistency
-              </span>
-              —our global in-house and partner facilities offer competitive
-              shipping rates and consistent quality.
-            </p>
-          </div>
           </div>
         </SwiperSlide>
       </Swiper>
